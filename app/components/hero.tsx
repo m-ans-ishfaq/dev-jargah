c6aa0785caf9577b17ee1cd5402ff74298692f04
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-08-04 11:11:50 +0500

    Merge issues

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { HeroBg } from "../assets/images";
-import { Header } from "./Header";
+import { Header } from "./header";
 import { content } from "../assets/data";
 import { FaLocationArrow, FaRegClock } from "react-icons/fa";
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Agenda } from "./components/agenda";
 import { Footer } from "./components/footer";
-import { Hero } from "./components/Hero";
+import { Hero } from "./components/hero";
 import { Hosts } from "./components/Hosts";
 import { QUA } from "./components/QUA";
 import { Speakers } from "./components/Speakers";
