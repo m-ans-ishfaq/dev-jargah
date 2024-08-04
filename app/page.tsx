374859ff3b5f137e344b8b4392969e5b4232e5fc
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-08-04 11:28:00 +0500

    Merge issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { Agenda } from "./components/agenda";
 import { Footer } from "./components/footer";
-import { Hero } from "./components/Hero";
+import { Hero } from "./components/hero";
 import { Hosts } from "./components/Hosts";
 import { QUA } from "./components/QUA";
-//import { Speakers } from "./components/Speakers";
+import { Speakers } from "./components/speakers";
 import { SponsorsAndOrganizers } from "./components/SponsorsAndOrganizers";
 
 export default function Home() {
