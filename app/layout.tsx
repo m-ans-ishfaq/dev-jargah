667b8ec09aef75d8f9aefa2c18348b460af71894
Ans <anasashfaq00@gmail.com>
2024-08-25 02:31:01 +0500

    Completed SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DEV JIRGA 24 | Embracing Innovation, Collaboration and Elevation",
-  description: "Embracing Innovation, Collaboration and Elevation",
+  description:
+    "Join us at Dev جرگہ Pakistan's Tech Event for an exciting day of innovation, knowledge exchange, and community spirit in Islamabad. Dive into the latest tech trends, interact with industry leaders through insightful discussions, and network with fellow tech enthusiasts shaping the future.",
+  keywords: [
+    "Dev Jirga 24",
+    "Dev Jargah",
+    "Devjirga",
+    "devjarga",
+    "Pakistan Tech Event",
+    "Innovation",
+    "Collaboration",
+    "Technology",
+    "Islamabad",
+    "Tech Conference",
+    "Community",
+    "Networking",
+  ],
 };
 
 export default function RootLayout({
@@ -17,7 +32,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full scroll-smooth bg-white antialiased">
       <head>
-        <link rel="icon" href="https://www.devjirga.live/_next/static/media/logo.735f2f8d.webp" />
+        <link
+          rel="icon"
+          href="https://www.devjirga.live/_next/static/media/logo.735f2f8d.webp"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
