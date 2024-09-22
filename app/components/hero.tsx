02c93f930c67daade43486135f9e2ac5d45cc21c
Ans <anasashfaq00@gmail.com>
2024-09-23 02:00:38 +0500

    minor changes

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,69 +3,67 @@ import { HeroBg } from "../assets/images";
 import { Header } from "./header";
 import { content } from "../assets/data";
 import { FaLocationArrow, FaRegClock } from "react-icons/fa";
+import Link from "next/link";
 
 export function Hero() {
-    return (
-        <section className="w-full overflow-x-hidden relative">
-            <Image src={HeroBg} alt="" className="absolute w-full h-full top-0 left-0 z-0 object-cover" />
-            <div className="absolute w-full h-full top-0 left-0 z-[1] bg-black bg-opacity-50 object-cover" />
-            {/* Circles */}
-            <div className="absolute -left-40 top-20 w-80 h-96 rounded-full z-[2] bg-gradient-radial from-[#FC343150] from-5% to-75% to-[#FC343100] opacity-50" />
-            <div className="absolute -right-40 top-60 w-80 h-96 rounded-full z-[2] bg-gradient-radial from-[#FC343150] from-5% to-75% to-[#FC343100] opacity-50" />
-            {/* Content */}
-            <div className="">
-                <Header transparent />
-                <div className="px-4 flex flex-col items-center">
-                    {/* Main Wrapper */}
-                    <div className="text-center max-w-5xl text-white relative z-[3] pt-16 sm:pt-40 pb-8 flex flex-col gap-4 items-center justify-center">
-                        <div className="space-y-2">
-                            <h1 className="text-xl sm:text-3xl font-bold">
-                                {content.title}
-                            </h1>
-                            <p className="text-justify sm:text-center text-neutral-300 text-lg">
-                                {content.description}
-                            </p>
-                        </div>
-                         <div className="flex justify-center">
-                            <div className="grid grid-cols-2 gap-4">
-                                
-                            
-                            </div>
-                       
-                    </div>
-                        <div className="">
-                            <div className="mt-16 w-full max-w-5xl py-6 bg-black bg-opacity-50 backdrop-blur-sm  rounded-md border-neutral-600 border flex flex-col gap-2 items-start px-6 text-start">
-                                <h3 className="text-xl">
-                                    {content.aboutUs.title}
-                                </h3>
-                                <p className="text-neutral-300">
-                                    {content.aboutUs.description}
-                                </p>
-                                <div className="mt-2 w-full flex flex-wrap gap-4 justify-between">
-                                    {/* Left */}
-                                    <div className="flex flex-wrap gap-6">
-                                        <div className="flex gap-2 items-center">
-                                            <FaRegClock />
-                                            <span>{content.eventDate} 09:00 AM</span>
-                                        </div>
-                                        <div className="flex gap-2 items-center">
-                                            <FaLocationArrow />
-                                            <span>{content.eventLocation}</span>
-                                        </div>
-                                    </div>
-                                    {/* Right */}
-                                    <div className="">
-                                        <button className="btn py-2 px-4">
-                                            View Speakers
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="h-20 bottom-0 bg-red-700 w-full absolute z-[2]" />
+  return (
+    <section className="w-full overflow-x-hidden relative">
+      <Image
+        src={HeroBg}
+        alt=""
+        className="absolute w-full h-full top-0 left-0 z-0 object-cover"
+      />
+      <div className="absolute w-full h-full top-0 left-0 z-[1] bg-black bg-opacity-50 object-cover" />
+      {/* Circles */}
+      <div className="absolute -left-40 top-20 w-80 h-96 rounded-full z-[2] bg-gradient-radial from-[#FC343150] from-5% to-75% to-[#FC343100] opacity-50" />
+      <div className="absolute -right-40 top-60 w-80 h-96 rounded-full z-[2] bg-gradient-radial from-[#FC343150] from-5% to-75% to-[#FC343100] opacity-50" />
+      {/* Content */}
+      <div className="">
+        <Header transparent />
+        <div className="px-4 flex flex-col items-center">
+          {/* Main Wrapper */}
+          <div className="text-center max-w-5xl text-white relative z-[3] pt-16 sm:pt-40 pb-8 flex flex-col gap-4 items-center justify-center">
+            <div className="space-y-2">
+              <h1 className="text-xl sm:text-3xl font-bold">{content.title}</h1>
+              <p className="text-justify sm:text-center text-neutral-300 text-lg">
+                {content.description}
+              </p>
             </div>
-        </section>
-    )
+            <div className="flex justify-center">
+              <div className="grid grid-cols-2 gap-4"></div>
+            </div>
+            <div className="">
+              <div className="mt-16 w-full max-w-5xl py-6 bg-black bg-opacity-50 backdrop-blur-sm  rounded-md border-neutral-600 border flex flex-col gap-2 items-start px-6 text-start">
+                <h3 className="text-xl">{content.aboutUs.title}</h3>
+                <p className="text-neutral-300">
+                  {content.aboutUs.description}
+                </p>
+                <div className="mt-2 w-full flex flex-wrap gap-4 justify-between">
+                  {/* Left */}
+                  <div className="flex flex-wrap gap-6">
+                    <div className="flex gap-2 items-center">
+                      <FaRegClock />
+                      <span>{content.eventDate} 09:00 AM</span>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <FaLocationArrow />
+                      <span>{content.eventLocation}</span>
+                    </div>
+                  </div>
+                  {/* Right */}
+                  <div className="flex gap-2">
+                    <button className="btn py-2 px-4">View Speakers</button>
+                    <Link href="/registration" className="btn py-2 px-4">
+                      Register Now
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-20 bottom-0 bg-red-700 w-full absolute z-[2]" />
+      </div>
+    </section>
+  );
 }
--- app/registration/form.tsx
+++ app/registration/form.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import { useFormik } from "formik";
+import React, { useState } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -13,172 +13,291 @@ const validationSchema = Yup.object({
   email: Yup.string()
     .email("Invalid email address")
     .required("Email is required"),
+  phone_number: Yup.string()
+    .matches(/^[0-9]{10,14}$/, "Phone number must be 10-14 digits")
+    .required("Phone number is required"),
+  university: Yup.string().required("University is required"),
+  city: Yup.string().required("City is required"),
+  cnic: Yup.string().required("CNIC is required"),
   qualification: Yup.string().required("Qualification is required"),
   linkedin_profile: Yup.string()
     .url("Invalid URL")
     .required("LinkedIn Profile Link is required"),
   why_you_want_to_attend_event: Yup.string().required("This field is required"),
-  have_you_been_referred_by_someone: Yup.string().required(
-    "This field is required"
-  ),
+  have_you_been_referred_by_someone: Yup.string(),
 });
 
 export function RegistrationForm() {
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      qualification: "",
-      linkedin_profile: "",
-      why_you_want_to_attend_event: "",
-      have_you_been_referred_by_someone: "",
-    },
-    validationSchema,
-    onSubmit: async (values, { resetForm }) => {
-      try {
-        console.log(values);
-        const docRef = await addDoc(collection(db, "registrations"), values);
-        await axios.post("/api/send-email", {
-          name: values.name,
-          email: values.email,
-        });
-        console.log("Document written with ID: ", docRef.id);
-        toast.success("Form submitted successfully");
-        resetForm();
-      } catch (e) {
-        console.error("Error adding document: ", e);
-        toast.error("Failed to submit form");
-      }
-    },
-  });
-
   return (
     <>
-      <form className="space-y-7" onSubmit={formik.handleSubmit}>
-        <div className="flex flex-col space-y-7 sm:flex-row sm:space-y-0 sm:space-x-6">
-          <div className="sm:w-1/2">
-            <label
-              htmlFor="name"
-              className="mb-3 block text-sm font-medium text-gray-700 after:absolute after:content-['*'] after:text-red-500"
-            >
-              Name
-            </label>
-            <input
-              id="name"
-              name="name"
-              type="text"
-              autoComplete="name"
-              className="block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              required
-            />
-          </div>
-          <div className="sm:w-1/2">
-            <label
-              htmlFor="email"
-              className="mb-3 block text-sm font-medium text-gray-700 after:absolute after:content-['*'] after:text-red-500"
-            >
-              Email address
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              className="block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              required
-            />
-          </div>
-        </div>
-        <div className="flex flex-col space-y-7 sm:flex-row sm:space-y-0 sm:space-x-6">
-          <div className="sm:w-1/2">
-            <label
-              htmlFor="qualification"
-              className="mb-3 block text-sm font-medium text-gray-700 after:absolute after:content-['*'] after:text-red-500"
-            >
-              Qualification
-            </label>
-            <select
-              id="qualification"
-              name="qualification"
-              className="block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-              value={formik.values.qualification}
-              onChange={formik.handleChange}
-              required
-            >
-              <option value="">Choose the option</option>
-              <option value="BSCS">BSCS</option>
-              <option value="BSSE">BSSE</option>
-              <option value="BS.IT">BS.IT</option>
-              <option value="MSCS">MSCS</option>
-              <option value="MCS">MCS</option>
-              <option value="MS.IT">MS.IT</option>
-              <option value="Others">Others</option>
-            </select>
-          </div>
-          <div className="sm:w-1/2">
-            <label
-              htmlFor="linkedin_profile"
-              className="mb-3 block text-sm font-medium text-gray-700 after:absolute after:content-['*'] after:text-red-500"
-            >
-              LinkedIn Profile Link
-            </label>
-            <input
-              id="linkedin_profile"
-              name="linkedin_profile"
-              type="url"
-              className="block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-              value={formik.values.linkedin_profile}
-              onChange={formik.handleChange}
-              required
-            />
-          </div>
-        </div>
-        <div>
-          <label
-            htmlFor="why_you_want_to_attend_event"
-            className="mb-3 block text-sm font-medium text-gray-700 after:absolute after:content-['*'] after:text-red-500"
-          >
-            Why you want to attend this event?
-          </label>
-          <textarea
-            id="why_you_want_to_attend_event"
-            name="why_you_want_to_attend_event"
-            className="resize-none block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-            value={formik.values.why_you_want_to_attend_event}
-            onChange={formik.handleChange}
-            required
-          ></textarea>
-        </div>
-        <div>
-          <label
-            htmlFor="have_you_been_referred_by_someone"
-            className="mb-3 block text-sm font-medium text-gray-700 after:absolute after:content-['*'] after:text-red-500"
-          >
-            Have you been referred by someone?
-          </label>
-          <textarea
-            id="have_you_been_referred_by_someone"
-            name="have_you_been_referred_by_someone"
-            className="resize-none block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-blue-500 focus:bg-white focus:outline-none focus:ring-blue-500 sm:text-sm"
-            value={formik.values.have_you_been_referred_by_someone}
-            onChange={formik.handleChange}
-            required
-          />
-        </div>
-        <div className="pt-1">
-          <button
-            className="group inline-flex items-center justify-center rounded-full py-3 px-5 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-brand-primary-600 text-white hover:text-slate-100 hover:bg-brand-primary-500 active:bg-brand-primary-800 active:text-brand-primary-100 focus-visible:outline-brand-primary-600 w-full"
-            type="submit"
-          >
-            Register
-          </button>
-        </div>
-      </form>
-      <ToastContainer position="bottom-right" />
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          phone_number: "",
+          university: "",
+          city: "",
+          cnic: "",
+          qualification: "",
+          linkedin_profile: "",
+          why_you_want_to_attend_event: "",
+          have_you_been_referred_by_someone: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={async (values, { resetForm }) => {
+          try {
+            const docRef = await addDoc(
+              collection(db, "registrations"),
+              values
+            );
+            await axios.post("/api/send-email", {
+              name: values.name,
+              email: values.email,
+            });
+            toast.success("Form submitted successfully");
+            resetForm();
+          } catch (e) {
+            console.error("Error adding document: ", e);
+            toast.error("Failed to submit form");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="space-y-7">
+            {/* Name and Email */}
+            <div className="flex flex-col space-y-7 sm:flex-row sm:space-y-0 sm:space-x-6">
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="name"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  Name
+                </label>
+                <Field
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Enter your name"
+                  className=" block w-full rounded-md border border-gray-200 px-3 py-2"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="email"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <Field
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  className="block w-full rounded-md border border-gray-200 px-3 py-2"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+            </div>
+
+            {/* Phone Number and CNIC */}
+            <div className="flex flex-col space-y-7 sm:flex-row sm:space-y-0 sm:space-x-6">
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="phone_number"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  Contact
+                </label>
+                <Field
+                  id="phone_number"
+                  name="phone_number"
+                  type="tel"
+                  placeholder="Enter your phone number"
+                  className=" block w-full rounded-md border border-gray-200 px-3 py-2"
+                />
+                <ErrorMessage
+                  name="phone_number"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="cnic"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  CNIC
+                </label>
+                <Field
+                  id="cnic"
+                  name="cnic"
+                  type="text"
+                  placeholder="Enter your CNIC"
+                  className="block w-full rounded-md border border-gray-200 px-3 py-2"
+                />
+                <ErrorMessage
+                  name="cnic"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+            </div>
+
+            {/* University and City */}
+            <div className="flex flex-col space-y-7 sm:flex-row sm:space-y-0 sm:space-x-6">
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="university"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  University
+                </label>
+                <Field
+                  id="university"
+                  name="university"
+                  type="text"
+                  placeholder="Enter your university"
+                  className="block w-full rounded-md border border-gray-200 px-3 py-2"
+                />
+                <ErrorMessage
+                  name="university"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="city"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  City
+                </label>
+                <Field
+                  id="city"
+                  name="city"
+                  type="text"
+                  placeholder="Enter your city"
+                  className="block w-full rounded-md border border-gray-200 px-3 py-2"
+                />
+                <ErrorMessage
+                  name="city"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+            </div>
+
+            {/* Qualification and LinkedIn */}
+            <div className="flex flex-col space-y-7 sm:flex-row sm:space-y-0 sm:space-x-6">
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="qualification"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  Qualification
+                </label>
+                <Field
+                  as="select"
+                  id="qualification"
+                  name="qualification"
+                  className="block w-full rounded-md border border-gray-200 px-3 py-2"
+                >
+                  <option value="">Choose an option</option>
+                  <option value="BSCS">BSCS</option>
+                  <option value="BSSE">BSSE</option>
+                  <option value="BS.IT">BS.IT</option>
+                  <option value="MSCS">MSCS</option>
+                  <option value="MCS">MCS</option>
+                  <option value="MS.IT">MS.IT</option>
+                  <option value="Others">Others</option>
+                </Field>
+                <ErrorMessage
+                  name="qualification"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+              <div className="sm:w-1/2">
+                <label
+                  htmlFor="linkedin_profile"
+                  className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+                >
+                  LinkedIn Profile Link
+                </label>
+                <Field
+                  id="linkedin_profile"
+                  name="linkedin_profile"
+                  type="url"
+                  placeholder="Enter your LinkedIn profile link"
+                  className="block w-full rounded-md border border-gray-200 px-3 py-2"
+                />
+                <ErrorMessage
+                  name="linkedin_profile"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+            </div>
+
+            {/* Why attend and Referral */}
+            <div>
+              <label
+                htmlFor="why_you_want_to_attend_event"
+                className="after:content-['*'] after:text-red-500 mb-3 block text-sm font-medium text-gray-700"
+              >
+                Why you want to attend this event?
+              </label>
+              <Field
+                as="textarea"
+                id="why_you_want_to_attend_event"
+                name="why_you_want_to_attend_event"
+                placeholder="Explain why you want to attend..."
+                className="block w-full rounded-md border border-gray-200 px-3 py-2"
+              />
+              <ErrorMessage
+                name="why_you_want_to_attend_event"
+                component="div"
+                className="text-red-500 text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="have_you_been_referred_by_someone"
+                className="mb-3 block text-sm font-medium text-gray-700"
+              >
+                Have you been referred by someone?
+              </label>
+              <Field
+                as="textarea"
+                id="have_you_been_referred_by_someone"
+                name="have_you_been_referred_by_someone"
+                placeholder="If yes, please specify..."
+                className="block w-full rounded-md border border-gray-200 px-3 py-2"
+              />
+            </div>
+
+            <div className="mt-5">
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="w-full rounded-full disabled:bg-red-700 bg-red-500 px-4 py-2 text-white"
+              >
+                {isSubmitting ? "Submitting..." : "Submit"}
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <ToastContainer />
     </>
   );
 }
